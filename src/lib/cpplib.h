02cc80062e19a0cdf6d1c72f60f65cfe9955b0ac
KaitaiZhang <kaitaizh@usc.edu>
2020-11-02 08:23:29 -0800

    Create Q1

--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -11,33 +11,19 @@
 class CPPLib {
  public:
   //q1
-  // Please create your functions here.
-  //q3
-  void SwapByRefernce(int &input1, int &input2);
-  void SwapByPointer(int *input1, int *input2);
-
-  //q4
-  void UniqeVectorNotBySet(std::vector<int> &input);
-  void UniqeVectorBySet(std::vector<int> &input);
-  void ReverseVector(std::vector<int> &input);
-  void OddVector(std::vector<int> &input);
-  std::vector<int> UnionVectors(std::vector<int> &input1, std::vector<int> &input2);
-
-  //q5
-  void ReverseString(std::string &input);
-  std::vector<int> ReverseVector_1(std::vector<int> input);
-  void ToLower(std::string& input);
-
-  //q6 
-  bool canBePalindrome(const std::string &str);
-
-  //q7
-  std::map<char, char> Mappable(const std::string& from, const std::string& to);
-
-  //q8
-  void kthPeek(std::vector<int> &input, int k);
+  int NumberOfNodes(TreeNode* root);
+  int SumOfNodes(TreeNode* root);
 
   private : 
+};
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 #endif
