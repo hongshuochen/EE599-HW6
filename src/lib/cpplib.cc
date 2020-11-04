1ba3c53f67caec2f3571d8c6acaa79001a1a1b41
hongshuochen <hongshuo@usc.edu>
2020-11-04 02:33:48 -0600

    q1

--- src/lib/cpplib.cc
+++ src/lib/cpplib.cc
@@ -2,21 +2,22 @@
 #include "limits"
 
 // Question 1
+bool CPPLib::canFinish(int numCourses, std::vector<std::pair<int, int>> &prerequisites) {
 
+}
 // Question 2
 
 // Question 3
 
-//Question 4
+// Question 4
 void SinglyLinkedList::sortList() {
     
 }
 
-//Question 5
-std::vector<char> ShortestPath(std::vector<char> &vertices,
+// Question 5
+std::vector<char> CPPLib::ShortestPath(std::vector<char> &vertices,
                                std::vector<std::pair<char, char>> &edges,
                                char node_1, char node_2) {
                         
 
 }
-
